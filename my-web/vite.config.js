f0e5c2af21e649e78c03052fbe9199d6d4899020
Kapil Kumar <kr_kapil@hotmail.com>
2023-10-05 19:30:36 +0530

    capacitor app with ionic cdn and htmx cdn

---
+++ my-web/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: '../my-app/dist',
+    minify: false,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        home: resolve(__dirname, 'home.html'),
+      }
+    }
+  },
+});
