cf277d96ddb9d3b1fa04debb7b4e77a3b25a5c15
Kapil Kumar <kr_kapil@hotmail.com>
2023-11-29 11:42:30 +0530

    seperate registration from service worker file

---
+++ my-web/register_wasm.js
@@ -0,0 +1,34 @@
+function registerWasmHTTPListener(wasm, { base, args = [] } = {}) {
+  let path = new URL(registration.scope).pathname
+  if (base && base !== '') path = `${trimEnd(path, '/')}/${trimStart(base, '/')}`
+
+  const handlerPromise = new Promise(setHandler => {
+    self.wasmhttp = {
+      path,
+      setHandler,
+    }
+  })
+
+  const go = new Go()
+  go.argv = [wasm, ...args]
+  WebAssembly.instantiateStreaming(fetch(wasm, { cache: "no-cache" }), go.importObject).then(({ instance }) => go.run(instance))
+
+  addEventListener('fetch', e => {
+    const { pathname } = new URL(e.request.url)
+    if (!pathname.startsWith(path)) return
+
+    e.respondWith(handlerPromise.then(handler => handler(e.request)))
+  })
+}
+
+function trimStart(s, c) {
+  let r = s
+  while (r.startsWith(c)) r = r.slice(c.length)
+  return r
+}
+
+function trimEnd(s, c) {
+  let r = s
+  while (r.endsWith(c)) r = r.slice(0, -c.length)
+  return r
+}
